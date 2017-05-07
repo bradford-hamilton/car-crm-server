cf6d3fe71615a6bc599d10d08d1501d7c4575013
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2017-05-07 16:31:58 -0600

    remove users all-together, as it was only part of initial setup

--- src/routes/api/v1/users/users.routes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import usersController from './users.controller';
-
-const router = express.Router();
-
-router.get('/get-all', usersController.getAll);
-
-export default router;
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,9 +1,7 @@
-import v1Users from './api/v1/users/users.routes';
 import v1Persons from './api/v1/persons/persons.routes';
 
 export default {
   init: (app) => {
-    app.use('/api/v1/users', v1Users);
     app.use('/api/v1/persons', v1Persons);
   },
 };
