0d090c56faa3aa244dbc567579101926dab730f7
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2017-05-10 22:21:56 -0600

    add production seeds

--- knexfile.js
+++ knexfile.js
@@ -42,7 +42,7 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: `${process.env.DATABASE_URL}ssl=require`,
+    connection: `${process.env.DATABASE_URL}?ssl=true`,
     migrations: {
       directory: `${__dirname}/db/migrations`,
     },
