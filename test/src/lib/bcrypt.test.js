67c2d143da5d730495905b93765b2c197eb7e3c5
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2017-05-19 13:14:43 -0600

    Add basic tests for crypt and jwt classes

--- test/src/lib/bcrypt.test.js
+++ test/src/lib/bcrypt.test.js
@@ -3,5 +3,8 @@ import Bcrypt from '../../../src/lib/bcrypt';
 require('../../test.setup')();
 
 describe('Bcrypt Class', () => {
-
+  let bcrypt = new Bcrypt();
+  it('bcrpyt class can be instantiated', () => {
+    expect(bcrypt).to.be.ok;
+  });
 });
--- test/src/lib/jwt.test.js
+++ test/src/lib/jwt.test.js
@@ -3,5 +3,8 @@ import Jwt from '../../../src/lib/jwt';
 require('../../test.setup')();
 
 describe('Jwt Class', () => {
-
+  let jwt = new Jwt();
+  it('bcrpyt class can be instantiated', () => {
+    expect(jwt).to.be.ok;
+  });
 });
